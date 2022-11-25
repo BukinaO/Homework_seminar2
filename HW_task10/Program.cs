78a207184c26ba960131b70c303f6821a3aaf0cb
BukinaO <117574860+BukinaO@users.noreply.github.com>
2022-11-25 12:33:03 +0300

    Updated

--- HW_task10/Program.cs
+++ HW_task10/Program.cs
@@ -1,9 +1,14 @@
-﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
 Console.Write("Введите число: ");
+
 int number = Convert.ToInt32(Console.ReadLine());
-int SecondDigit = number/10;
+
+int SecondDigit = number/10%10;
+
 if(number > 99 && number < 1000)
+
 {
+
     Console.Write(SecondDigit);
 }
-
